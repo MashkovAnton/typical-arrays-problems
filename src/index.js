3af9bb7a19ecb3927c0624315c0c386ad55b91e3
Anton Mashkov <mashkovv.anton@gmail.com>
2020-10-02 00:31:22 +0300

    feat: functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+  let num = array[0];
+  for( let i = 0; i < array.length; i++) {
+    if(num > array[i]){
+      num = array[i]
+    }
+  }
+  return num;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+  let num = array[0];
+  for( let i = 0; i < array.length; i++) {
+    if(num < array[i]){
+      num = array[i]
+    }
+  }
+  return num;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for( let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum/array.length;
 }
